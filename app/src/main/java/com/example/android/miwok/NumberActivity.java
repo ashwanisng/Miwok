679b29d87dd2920faea7b3b9434178a0b03203fe
ashwanisng <ashwanicena5@gmail.com>
2020-04-13 08:24:48 +0530

    ui has added

---
+++ app/src/main/java/com/example/android/miwok/NumberActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.miwok;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class NumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_number);
+    }
+
+
+
+
+}
